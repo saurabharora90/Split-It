55f85a6e9cdbfbe66ebde3d9dbe697975b24ae13
Saurabh <saurabharora90@gmail.com>
2016-04-04 18:48:58 +0800

    uwp project setup

---
+++ Split_It/Split_It/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_It.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
---
+++ Split_It/Split_It/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,47 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_It.ViewModel
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+                //SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
+            }
+            else
+            {
+                //SimpleIoc.Default.Register<IDataService, DataService>();
+            }
+
+            SimpleIoc.Default.Register<IDialogService, DialogService>();
+            SimpleIoc.Default.Register<INavigationService, NavigationService>();
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the MainVM property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainViewModel MainVM
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+    }
+}
