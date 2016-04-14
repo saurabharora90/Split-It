ad5a39646b25519e16c9de3158af136ae7b7542e
Saurabh <saurabharora90@gmail.com>
2016-04-15 01:57:19 +0800

    show comments for expense

---
+++ Split_It/Split_It/Converter/Comment/CommentTypeFontWeightConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Text;
+using Windows.UI.Xaml.Data;
+
+namespace Split_It.Converter.Comment
+{
+    class CommentTypeFontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value.ToString().Equals("system", StringComparison.CurrentCultureIgnoreCase))
+                return FontWeights.Light;
+            else
+                return FontWeights.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Split_It/Split_It/Converter/Comment/CommentTypeVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Split_It.Converter.Comment
+{
+    class CommentTypeVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value.ToString().Equals("system", StringComparison.CurrentCultureIgnoreCase))
+                return Visibility.Collapsed;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
