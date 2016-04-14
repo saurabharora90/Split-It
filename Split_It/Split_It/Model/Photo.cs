6d7a501ea91fca9b3af6c89f822437d5833cf8fa
Saurabh <saurabharora90@gmail.com>
2016-04-15 00:16:18 +0800

    clean up code

--- Split_It/Split_It/Model/Photo.cs
+++ Split_It/Split_It/Model/Photo.cs
@@ -1,43 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Split_It.Model
+﻿namespace Split_It.Model
 {
+    /// <summary>
+    /// We don't need the samll and large photos. Uselessly taking up memory.
+    /// </summary>
     public class Photo : GalaSoft.MvvmLight.ObservableObject
     {
-        /// <summary>
-        /// The <see cref="Small" /> property's name.
-        /// </summary>
-        public const string SmallPropertyName = "Small";
-
-        private string _small = null;
-
-        /// <summary>
-        /// Sets and gets the Small property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        /// 
-        public string Small
-        {
-            get
-            {
-                return _small;
-            }
-
-            set
-            {
-                if (_small == value)
-                {
-                    return;
-                }
-
-                _small = value;
-                RaisePropertyChanged(SmallPropertyName);
-            }
-        }
 
         /// <summary>
         /// The <see cref="Medium" /> property's name.
@@ -66,36 +33,6 @@ namespace Split_It.Model
 
                 _medium = value;
                 RaisePropertyChanged(MediumPropertyName);
-            }
-        }
-
-        /// <summary>
-        /// The <see cref="Large" /> property's name.
-        /// </summary>
-        public const string LargePropertyName = "Large";
-
-        private string _large = null;
-
-        /// <summary>
-        /// Sets and gets the Large property.
-        /// Changes to that property's value raise the PropertyChanged event. 
-        /// </summary>
-        public string Large
-        {
-            get
-            {
-                return _large;
-            }
-
-            set
-            {
-                if (_large == value)
-                {
-                    return;
-                }
-
-                _large = value;
-                RaisePropertyChanged(LargePropertyName);
             }
         }
 
