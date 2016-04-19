f2112ea323d7fffaa39a06f9f6e632e7bbd1b128
Saurabh <saurabharora90@gmail.com>
2016-04-19 23:55:27 +0800

    clean up and styling

--- Split_It/Split_It/Converter/AddExpense/SelectionToMemberConverter.cs
+++ Split_It/Split_It/Converter/AddExpense/SelectionToMemberConverter.cs
@@ -5,19 +5,23 @@ using Windows.UI.Xaml.Data;
 
 namespace Split_It.Converter.AddExpense
 {
-    public class GroupSelectedToMembersConverter : IValueConverter
+    public class SelectionToMemberConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             var e = value as SelectionChangedEventArgs;
             if (e.AddedItems.Count == 0)
                 return null;
-
-            var group = e.AddedItems[0] as Group;
-            if (group.Id == 0)
-                return null;
+            if(e.AddedItems[0] is Group)
+            {
+                var group = e.AddedItems[0] as Group;
+                if (group.Id == 0)
+                    return null;
+                else
+                    return group.Members;
+            }
             else
-                return group.Members;
+                return e.AddedItems[0] as ExpenseUser;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
