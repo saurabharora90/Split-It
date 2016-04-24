2a068fd552211c5a01fc6c4c8cd3f1dad66c949c
Saurabh <saurabharora90@gmail.com>
2016-04-25 01:06:38 +0800

    IAP

--- Split_It/Split_It/ViewModel/SideMenuViewModel.cs
+++ Split_It/Split_It/ViewModel/SideMenuViewModel.cs
@@ -1,19 +1,36 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using Split_It.Utils;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Store;
 using Windows.System;
 
 namespace Split_It.ViewModel
 {
     public class SideMenuViewModel : ViewModelBase
     {
+        LicenseInformation licenseInformation;
+
         public SideMenuViewModel()
         {
-
+#if DEBUG
+            licenseInformation = CurrentAppSimulator.LicenseInformation;
+#else
+            licenseInformation = CurrentApp.LicenseInformation;
+#endif
+            if (licenseInformation.ProductLicenses[Constants.REMOVE_ADS_NEW_PRODUCT_ID].IsActive || licenseInformation.ProductLicenses[Constants.REMOVE_ADS_OLD_PRODUCT_ID].IsActive)
+            {
+                IsAdsEnabled = false;
+            }
+            else
+            {
+                IsAdsEnabled = true;
+            }
         }
 
         /// <summary>
@@ -75,9 +92,26 @@ namespace Split_It.ViewModel
             {
                 return _removeAdsCommand
                     ?? (_removeAdsCommand = new RelayCommand(
-                    () =>
+                    async () =>
                     {
+                        try
+                        {
+                            // The customer doesn't own this feature, so 
+                            // show the purchase dialog.
 
+                            string code = await CurrentAppSimulator.RequestProductPurchaseAsync(Constants.REMOVE_ADS_NEW_PRODUCT_ID, false);
+                            //Check the license state to determine if the in-app purchase was successful.
+                            if (licenseInformation.ProductLicenses[Constants.REMOVE_ADS_NEW_PRODUCT_ID].IsActive)
+                            {
+                                IsAdsEnabled = false;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            // The in-app purchase was not completed because 
+                            // an error occurred.
+                            Debug.Write(e);
+                        }
                     }));
             }
         }
